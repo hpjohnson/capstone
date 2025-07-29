41d5a8a94b1fdcd86eccec37ec7145ce5e3f45f3
hpjohnson <hpjohnson0211@gmail.com>
2025-07-29 13:37:00 +0100

    add log in, log out, sign up pages and enable logging in

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('about/', include('about.urls'), name='about_urls'),
+    path('accounts/', include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('question/', include('question.urls'), name="question_urls"),
     path('summernote/', include('django_summernote.urls')),
