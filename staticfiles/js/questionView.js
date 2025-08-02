3048e6575352443b597df08f2bec08b6f1baf466
hpjohnson <hpjohnson0211@gmail.com>
2025-08-02 18:39:39 +0100

    force staticfiles to hopefully fix heroku deploy being broken?

---
+++ staticfiles/js/questionView.js
@@ -0,0 +1,78 @@
+/**
+ * run on page load
+ */
+document.addEventListener("DOMContentLoaded", function () {
+    
+    viewQuestion()
+
+})
+
+/**
+ * add event listeners and functionality to all buttons
+ */
+function viewQuestion() {
+
+    const questionButtons = document.getElementsByClassName("question-view-button");
+
+    for (let questionButton of questionButtons) {
+        
+        questionButton.addEventListener("click", function () {
+            
+            // get the question card
+            const questionCard = this.parentElement.parentElement.parentElement;
+            
+            // get the content from the card
+            const questionTitle = questionCard.querySelector(".question-title").textContent;
+            const questionSubtitle = questionCard.querySelector(".question-subtitle").textContent;
+            const questionContent = questionCard.querySelector(".question-content").textContent;
+            const questionDifficulty = questionCard.querySelector(".question-difficulty").textContent;
+
+            // get the content from the data attributes
+            const questionDateCreated = questionCard.getAttribute("data-created-on");
+            const questionDateUpdated = questionCard.getAttribute("data-last-updated");
+            const questionUpdateCount = questionCard.getAttribute("data-update-count");
+
+            // set data in the modal
+            document.querySelector(".question-view-title").innerHTML = questionTitle;
+            document.querySelector(".question-view-subtitle").innerHTML = questionSubtitle;
+            document.querySelector(".question-view-difficulty").innerHTML = questionDifficulty
+            document.querySelector(".question-view-content").innerHTML = questionContent;
+            document.querySelector(".question-view-date-created").innerHTML = `Date Posted: ${questionDateCreated}`;
+            document.querySelector(".question-view-date-updated").innerHTML = `Last Update: ${questionDateUpdated}`;
+            document.querySelector(".question-view-update-count").innerHTML = `Update Count: ${questionUpdateCount}`;
+
+            // set the correct class for the difficulty
+            const questionDifficultyElement = document.querySelector(".question-view-difficulty");
+
+            // list of classes
+            const DIFFICULTYCLASSLIST = ["question-easy", "question-medium", "question-hard", "question-insane", "question-none"];
+
+            // remove the class
+            for (difficulty of DIFFICULTYCLASSLIST) {
+                questionDifficultyElement.classList.remove(`${difficulty}`);
+            }
+
+            // add the correct class
+            switch (questionCard.getAttribute("data-difficulty")) {
+                case "-1":
+                    questionDifficultyElement.classList.add(`question-none`);
+                    break;
+                case "0":
+                    questionDifficultyElement.classList.add(`question-easy`);
+                    break;
+                case "1":
+                    questionDifficultyElement.classList.add(`question-medium`);
+                    break;
+                case "2":
+                    questionDifficultyElement.classList.add(`question-hard`);
+                    break;
+                case "3":
+                    questionDifficultyElement.classList.add(`question-insane`);
+                    break;
+            }         
+
+        })
+
+    }
+
+}
