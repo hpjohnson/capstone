17146bc8254e185df5d45c820e8b662c322ef436
hpjohnson <hpjohnson0211@gmail.com>
2025-08-02 13:26:28 +0100

    add functionality to get users uploaded questions on manage questions page

--- manageQuestions/views.py
+++ manageQuestions/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from question.models import Question
 
 # Create your views here.
 def manage_questions_page_view(request):
 
-    return render(request, "manageQuestions/manageQuestions.html", )
+    # get the users uploaded questions
+    questions = Question.objects.filter(userID=request.user)
+    
+    # testing stuff, delete this later
+    # print(questions)
+    # print(questions[0].questionID)
+
+    return render(request, "manageQuestions/manageQuestions.html", {"questions": questions}, )
